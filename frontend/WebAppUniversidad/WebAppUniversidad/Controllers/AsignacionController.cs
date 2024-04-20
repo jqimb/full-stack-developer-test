0193f3b063cf2480a70a50eee5a1a760c2bd1cec
codejq <codejq@mail.com>
2024-04-20 13:33:44 -0600

    Se agrega proyecto frontend requerido

---
+++ frontend/WebAppUniversidad/WebAppUniversidad/Controllers/AsignacionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppUniversidad.Controllers
+{
+    public class AsignacionController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public AsignacionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            // Se busca la configuracion del URL base de las APIs
+            string apiURL = _configuration["UrlAPIBackend"] ?? "";
+            ViewData["apiURL"] = apiURL;
+            return View();
+        }
+    }
+}
---
+++ frontend/WebAppUniversidad/WebAppUniversidad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppUniversidad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
